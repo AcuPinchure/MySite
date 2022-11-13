85b9a4b3ee2974d5b0f7a5df2dd7e554ca987ea1
AcuPinchure <s920069168@gmail.com>
2022-11-13 17:00:58 +0800

    prepare for apache

--- src/MySite/wsgi.py
+++ src/MySite/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
+project_home = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_home not in sys.path:
+    # print(project_home)
+    sys.path.append(project_home)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MySite.settings')
 
 application = get_wsgi_application()
--- src/logistics/urls.py
+++ src/logistics/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = "logistics"
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    re_path(r'^.?',RedirectView.as_view(pattern_name='logistics:home', permanent=False))
 ]
