a7515cb224651ab6b10603cfed9b6eec29c2decd
AcuPinchure <s920069168@gmail.com>
2022-11-13 22:06:56 +0800

    static manage

--- src/logistics/urls.py
+++ src/logistics/urls.py
@@ -5,5 +5,9 @@ from django.views.generic import RedirectView
 app_name = "logistics"
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('account/', views.account, name='account'),
+    path('warehouse/', views.warehouse, name='warehouse'),
+    path('order/', views.order, name='order'),
+    path('delivery/', views.delivery, name='delivery'),
     re_path(r'^.?',RedirectView.as_view(pattern_name='logistics:home', permanent=False))
 ]
