247c119252bf8f5ebcc77a88f21f215a6bc4dc6c
AcuPinchure <s920069168@gmail.com>
2022-11-14 17:08:23 +0800

    add order UI

--- src/logistics/urls.py
+++ src/logistics/urls.py
@@ -3,11 +3,18 @@ from . import views
 from django.views.generic import RedirectView
 
 app_name = "logistics"
+
+order_patterns = [
+    path('', views.order, name='order'),
+    path('edit/<str:order_uuid>/', views.orderEdit, name='order_edit'),
+]
+
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('account/', views.account, name='account'),
     path('warehouse/', views.warehouse, name='warehouse'),
-    path('order/', views.order, name='order'),
+    path('order/', include(order_patterns)),
     path('delivery/', views.delivery, name='delivery'),
-    re_path(r'^.?',RedirectView.as_view(pattern_name='logistics:home', permanent=False))
+    re_path(r'^.?', RedirectView.as_view(
+        pattern_name='logistics:home', permanent=False))
 ]
--- src/logistics/views.py
+++ src/logistics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,14 +6,29 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'logistics/home.html')
 
+
 def account(request):
     return render(request, 'logistics/account.html')
+
 
 def warehouse(request):
     return render(request, 'logistics/warehouse.html')
 
+
 def order(request):
-    return render(request, 'logistics/order.html')
+    if request.method == "POST":
+        post = request.POST
+        action = list(post.keys())[-1]
+        if action == "new_order":
+            return redirect("logistics:order_edit", order_uuid="1234")
+    return render(request, 'logistics/order/order.html')
+
+
+def orderEdit(request, order_uuid):
+    return render(request, 'logistics/order/order_edit.html', {
+        "order_id": order_uuid
+    })
+
 
 def delivery(request):
     return render(request, 'logistics/delivery.html')
