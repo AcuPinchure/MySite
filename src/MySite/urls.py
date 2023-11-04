0136142b52174b3e308237ea8d9b310d346b6da1
AcuPinchure <s920069168@gmail.com>
2023-11-04 22:37:11 +0800

    [local_auth] add url that redirect unregister url to local_auth.views.notFound

--- src/MySite/urls.py
+++ src/MySite/urls.py
@@ -18,14 +18,18 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
-from main import views
+from local_auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('auth/', include('local_auth.urls')),
-    path('main/',include('main.urls')),
-    path('bot/',include('twitter_bot.urls')),
+    path('bot/', include('twitter_bot.urls')),
     # path('logistics/',include('logistics.urls')),
-    #path('.well-known/pki-validation/<str:file_name>/',views.sslValidation) # SSL validation
-    #re_path(r'',RedirectView.as_view(url='bot/index/'))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # path('.well-known/pki-validation/<str:file_name>/',views.sslValidation) # SSL validation
+    # re_path(r'',RedirectView.as_view(url='bot/index/'))
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    re_path(r'.*', views.notFound),
+]
